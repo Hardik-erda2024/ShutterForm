47fff13ef5d7085b6390a98b44a24c93d06324e2
hardik.erda <hardik.erda@esparkbizmail.com>
2024-07-23 13:42:59 +0530

    init commit

---
+++ app/RTK/store.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../RTK/Features/demo/demoSlice";
+import customerReducer from "./Features/customer/customerSlice";
+import shutterSlice from "./Features/shutter/shutterSlice";
+import shutterSellSlice from "./Features/shutterSell/shutterSellSlice";
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  customerDetails: customerReducer,
+  shutterList:shutterSlice,
+  shutterSellList:shutterSellSlice
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
+export type RootState = ReturnType<typeof store.getState>
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import MainForm from "./component/MainForm";
+
+export default function Home() {
+  return (
+      <MainForm />
+  );
+}
