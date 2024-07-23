fe5258c4435207496007fd502318ac8802f1a2c5
hardik.erda <hardik.erda@esparkbizmail.com>
2024-07-23 14:08:52 +0530

    default values added to redux

--- app/RTK/Features/customer/customerSlice.ts
+++ app/RTK/Features/customer/customerSlice.ts
@@ -1,18 +1,29 @@
-"use client"
+"use client";
 
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [{
-    modal:{Name:"Hardik",Email:"hardik@gmail.com",Phone:"6355395891"}
-}]
+const initialState = [
+  {
+    modal: { Name: "Hardik", Email: "hardik@gmail.com", Phone: "6355395891" },
+  },
+  {
+    modal: { Name: "Raj", Email: "raj@gmail.com", Phone: "6875958910" },
+  },
+  {
+    modal: { Name: "Jayraj", Email: "hardik@gmail.com", Phone: "8547699856" },
+  },
+  {
+    modal: { Name: "Kishan", Email: "hardik@gmail.com", Phone: "9988776655" },
+  },
+];
 export const customerSlice = createSlice({
-    name:"customerDetails",
-    initialState,
-    reducers:{
-        addCustomerDetails:(state,action)=>{
-            state.push(action.payload)
-        }
-    }
-})
-export const {addCustomerDetails} = customerSlice.actions
-export default customerSlice.reducer
+  name: "customerDetails",
+  initialState,
+  reducers: {
+    addCustomerDetails: (state, action) => {
+      state.push(action.payload);
+    },
+  },
+});
+export const { addCustomerDetails } = customerSlice.actions;
+export default customerSlice.reducer;
