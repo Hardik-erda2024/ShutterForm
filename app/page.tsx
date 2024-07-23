33c994b93f59539e37242a6f19270fb6a74ea299
hardik.erda <hardik.erda@esparkbizmail.com>
2024-07-23 13:51:51 +0530

    suspense added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 "use client";
+import { Suspense } from "react";
 import MainForm from "./component/MainForm";
 
 export default function Home() {
   return (
+    <Suspense fallback={null}>
       <MainForm />
+    </Suspense>
   );
 }
